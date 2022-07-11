7c716378d870efbee9e4d1ab978f3fbb0d5e3e2e
bizip <bizip04@gmail.com>
2022-07-11 16:29:17 +0200

    Crestte database with a table contained with some data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
-
+-- animal table
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg FLOAT(5),
+    PRIMARY KEY(id)
 );
+
