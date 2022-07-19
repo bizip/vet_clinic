2d182e0cbe27cf5054484bdc33f1407816055cc9
myelin0 <emi.veisllari@gmail.com>
2022-07-19 15:51:00 +0200

    Add Table and join tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,61 @@ CREATE DATABASE clinic;
             REFERENCES patients(id) 
             ON DELETE CASCADE
     );
+
+    CREATE TABLE treatments ( 
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        type VARCHAR(200),
+        name VARCHAR(200)
+    );
+
+    CREATE TABLE treatments_histories ( 
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        medical_history_id INT,
+        treatment_id INT,
+        CONSTRAINT fk_medical_history_id 
+            FOREIGN KEY (medical_history_id) 
+            REFERENCES medical_histories(id) 
+            ON DELETE CASCADE,
+        CONSTRAINT fk_treatment_id 
+            FOREIGN KEY (treatment_id) 
+            REFERENCES treatments(id) 
+            ON DELETE CASCADE
+    );
+
+    CREATE TABLE invoices ( 
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        total_amount DECIMAL,
+        generated_at TIMESTAMP,
+        payed_at TIMESTAMP,
+        medical_history_id INTEGER,
+        CONSTRAINT unique_history_id 
+                UNIQUE(medical_history_id),
+        CONSTRAINT fk_medical_history_id 
+            FOREIGN KEY (medical_history_id) 
+            REFERENCES medical_histories(id) 
+            ON DELETE CASCADE
+    );
+
+     CREATE TABLE invoice_items ( 
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        unit_price DECIMAL,
+        quantity INT,
+        total_price DECIMAL,
+        invoice_id INTEGER,
+        treatment_id INTEGER,
+        CONSTRAINT fk_invoices_id 
+            FOREIGN KEY (invoice_id) 
+            REFERENCES invoices(id) 
+            ON DELETE CASCADE,
+        CONSTRAINT fk_treatment_id 
+            FOREIGN KEY (treatment_id) 
+            REFERENCES treatments(id) 
+            ON DELETE CASCADE
+    );
+
+CREATE INDEX  patient_id_index ON medical_histories(patient_id);
+CREATE INDEX  invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX  treatment_id_index ON invoice_items(treatment_id);
+CREATE INDEX  medical_id_index ON invoices(medical_history_id);
+CREATE INDEX  treatment_id_history_index ON treatments_histories(treatment_id);
+CREATE INDEX  medical_history_id_index ON treatments_histories(medical_history_id);
